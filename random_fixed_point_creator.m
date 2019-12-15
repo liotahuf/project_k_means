43ceb4c10ed73cacdfab40141a8a87200090ea5c
liotahuf <liorahuf@gmail.com>
2019-12-15 16:31:54 +0200

    matlab models added

--- random_fixed_point_creator.m
+++ random_fixed_point_creator.m
@@ -6,3 +6,11 @@ cnt = randi(20);
 div = rand_num/cnt;
 div_fixed = fi(div,1,22,10) 
 bin(div_fixed)
+
+rand_num =-2 + 4*rand;
+fixed_point_rand = fi(-1223,1,22,10) 
+bin(fixed_point_rand)
+cnt = 390;
+div = fixed_point_rand/cnt;
+div_fixed = fi(div,1,22,10) 
+bin(div_fixed)
