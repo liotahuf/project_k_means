4aed3b6a147afd11fad59d3cfc85cd43518b18a0
liotahuf <liorahuf@gmail.com>
2020-01-05 18:54:34 +0200

    controller new block diagram added

--- random_fixed_point_creator.m
+++ random_fixed_point_creator.m
@@ -14,3 +14,8 @@ cnt = 390;
 div = fixed_point_rand/cnt;
 div_fixed = fi(div,1,22,10) 
 bin(div_fixed)
+
+%% convergence check fixed point creator
+rand_num =-2 + 4*rand;
+fixed_point_rand = fi(rand_num,1,22,10); 
+bin(fixed_point_rand);
